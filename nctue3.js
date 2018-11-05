0b5673cdfb9b9fcb99931749082631b47ca9595e
gamerslouis <gamerslouis@gmail.com>
2018-11-05 19:33:52 +0800

    no message

---
+++ nctue3.js
@@ -0,0 +1,9 @@
+"strict";
+
+let id_ = 'ctl00_ContentPlaceHolder1_Stu_TeamDetailControl1_rpTeams_ctl02_divTeamMember';
+
+fetch(chrome.extension.getURL('https://gamerslouis.github.io/nctue3.html')).then((res) => {
+    return res.text();
+}).then((content) => {
+    document.getElementById(id_).innerHTML = content;
+});
