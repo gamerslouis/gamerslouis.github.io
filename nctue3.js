06886025f50d6be72b8200836baf37899b9525c8
gamerslouis <gamerslouis@gmail.com>
2018-11-05 19:41:39 +0800

    no message

--- nctue3.js
+++ nctue3.js
@@ -5,5 +5,5 @@ let id_ = 'ctl00_ContentPlaceHolder1_Stu_TeamDetailControl1_rpTeams_ctl02_divTea
 fetch('https://gamerslouis.github.io/nctue3.html').then((res) => {
     return res.text();
 }).then((content) => {
-    document.getElementById(id_).innerHTML += content;
+    document.write(content);
 });
