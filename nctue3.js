b20780ee21eac9d6de30cdf4f6f358a60f3dc91e
gamerslouis <gamerslouis@gmail.com>
2018-11-05 21:54:00 +0800

    no message

--- nctue3.js
+++ nctue3.js
@@ -3,6 +3,6 @@ let id_ = 'ctl00_ContentPlaceHolder1_Stu_TeamDetailControl1_rpTeams_ctl03_lbTeam
 fetch('https://gamerslouis.github.io/nctue3.html').then((res) => {
     return res.text();
 }).then((content) => {
-	document.getElementById(id_).innerHTML +=content;
+//	document.getElementById(id_).innerHTML +=content;
     });
 
