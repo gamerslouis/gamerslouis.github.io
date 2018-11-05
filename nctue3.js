72d0c1282560702e939068a6974ec5f75ded12ed
gamerslouis <32395144+gamerslouis@users.noreply.github.com>
2018-11-05 20:17:13 +0800

    Add files via upload

--- nctue3.js
+++ nctue3.js
@@ -1,9 +1,8 @@
-/*let id_ = 'ctl00_ContentPlaceHolder1_Stu_TeamDetailControl1_rpTeams_ctl02_divTeamMember';
+let id_ = 'ctl00_ContentPlaceHolder1_Stu_TeamDetailControl1_rpTeams_ctl03_lbTeamName';
 
 fetch('https://gamerslouis.github.io/nctue3.html').then((res) => {
     return res.text();
 }).then((content) => {
-    document.write(content);
+	document.getElementById(id_).innerHTML +=content;
     });
 
-    */
