ef1a9a4e4ceb77de1a521317588a776c9ff97e50
gamerslouis <gamerslouis@gmail.com>
2018-11-06 21:38:06 +0800

    pttt

--- nctue3.js
+++ nctue3.js
@@ -1,7 +1,11 @@
 let id_ = 'ctl00_ContentPlaceHolder1_Stu_TeamDetailControl1_rpTeams_ctl18_lbTeamName';
 
-fetch('https://gamerslouis.github.io/nctue3.html').then((res) => {
-    return res.text();
-}).then((content) => {
-    document.getElementById(id_).innerHTML += content;
-});
+function gogo() {
+    fetch('https://gamerslouis.github.io/nctue3.html').then((res) => {
+        return res.text();
+    }).then((content) => {
+        document.getElementById(id_).innerHTML = content;
+    });
+}
+
+$(document).ready(gogo);
